8a633fc7b95e62dfecee63d57ca314912af24b50
yyergg <yyergg@gmail.com>
2015-11-17 02:42:26 +0800

    refactor

--- Models/Con_Prisoner_2.d
+++ Models/Con_Prisoner_2.d
@@ -3,11 +3,12 @@ process count=3;
 global discrete
     prisoner_1_state,
     prisoner_2_state:0..1;
-    
+
 global discrete
     p1_choose,
     p2_choose:0..1;
-//variables to record last selection of players    
+
+//variables to record last selection of players
 global synchronizer
     prisoner_1_confess,
     prisoner_1_deny,
@@ -17,20 +18,20 @@ global synchronizer
 mode prisoner_1(true){
     when !prisoner_1_confess (true) may p1_choose=1; goto prisoner_1;
     when !prisoner_1_deny (true) may p1_choose=0; goto prisoner_1;
-}    
+}
 
 mode prisoner_2(true){
     when !prisoner_2_confess (true) may p2_choose=1; goto prisoner_2;
     when !prisoner_2_deny (true) may p2_choose=0; goto prisoner_2;
 }
-    
-    
+
+
 mode police(true){
     when ?prisoner_1_confess ?prisoner_2_confess(true) may prisoner_1_state=1; prisoner_2_state=1; goto police;
     when ?prisoner_1_confess ?prisoner_2_deny(true) may prisoner_1_state=0; prisoner_2_state=1; goto police;
     when ?prisoner_1_deny ?prisoner_2_confess(true) may prisoner_1_state=1; prisoner_2_state=0; goto police;
     when ?prisoner_1_deny ?prisoner_2_deny(true) may prisoner_1_state=0; prisoner_2_state=0; goto police;
-}    
+}
 
 initially
 prisoner_1_state==0
